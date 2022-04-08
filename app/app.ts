cddd27484a2add5087f313256765f77748b1aeb3
Samir Mirzaliyev <themirzaliyev@Samirs-MacBook-Air.local>
2022-04-08 20:02:18 +0400

    Updated

--- app/app.ts
+++ app/app.ts
@@ -29,12 +29,12 @@ const startServer = async (): Promise<void> => {
   }
 
   // Production Deploy
-  if (process.env.NODE_ENV === 'production') {
+  // if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
     app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
     });
-  }
+  // }
 
   // app.all('*', (req, res, next) => {
   //   res.header(
