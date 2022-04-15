48a24620696bb9cacd9d2fd7b3adb5d64e4153ff
Samir Mirzaliyev <themirzaliyev@Samirs-MacBook-Air.local>
2022-04-15 11:34:42 +0400

    server problem

--- client/src/infrastructure/global/axios/axios.ts
+++ client/src/infrastructure/global/axios/axios.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:4000/api',
-    // process.env.NODE_ENV === 'development'
-    //   ? 'http://localhost:4000/api'
-    //   : 'https://themrzlyv.herokuapp.com/api',
+  baseURL: 
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:4000/api'
+      : 'https://themrzlyv.herokuapp.com/api',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
