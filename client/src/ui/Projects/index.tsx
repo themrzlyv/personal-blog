5a29af23d720422476843529844fe2cd3f8f4065
Samir Mirzaliyev <themirzaliyev@Samirs-MacBook-Air.local>
2022-04-30 00:55:59 +0400

    projects ready

--- client/src/ui/Projects/index.tsx
+++ client/src/ui/Projects/index.tsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from 'react';
+import { Chrono } from 'react-chrono';
+import { TimelineItemModel } from 'react-chrono/dist/models/TimelineItemModel';
 
 const Projects = () => {
-  return (
-    <div>Projects</div>
-  )
-}
+  const items: TimelineItemModel[] = [
+    {
+      cardTitle: 'Personal Blog',
+      cardSubtitle: 'Full-Stack Web Application',
+      cardDetailedText: 'React, React Hooks, Typescript, Tailwind, Express, Prisma, PostgreSQL',
+      position: 'left',
+    },
+    {
+      cardTitle: 'Authline Car Rental',
+      cardSubtitle: 'Full-Stack',
+      cardDetailedText: 'React, Typescript, Mui, Express, Prisma, MongoDB',
+      position: 'left',
+    },
+    {
+      cardTitle: 'E-Commerce Website',
+      cardSubtitle: 'Full-Stack',
+      cardDetailedText: 'React, Typescript, Redux, Formik, Express, MongoDB',
+      position: 'left',
+    },
+    {
+      cardTitle: 'Flexline Blog',
+      cardSubtitle: 'Full-Stack',
+      cardDetailedText: 'React, Typescript, Redux, Formik, Bootstrap 5, Express, MongoDB',
+      position: 'left',
+    },
+    {
+      cardTitle: 'Movieland Website',
+      cardSubtitle: 'Front-End',
+      cardDetailedText: 'Next Js, Context Api, Sass, TMDB Api',
+      position: 'left',
+    },
+  ];
 
-export default Projects
+  return (
+    <div className='w-full py-5 my-5 dark:bg-zinc-900 shadow-md'>
+      <Chrono
+        items={items}
+        hideControls
+        slideShow
+        slideItemDuration={4500}
+        lineWidth={5}
+        theme={{ primary: 'gray', secondary: 'cyan', titleColor: 'white' }}
+      />
+    </div>
+  );
+};
+
+export default Projects;
